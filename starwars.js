7cdfc36a8eda29446129eb71079e2a5178208f8d
Felipe Freitas <felipesafreitas@gmail.com>
2018-04-26 21:51:40 -0300

    added changes on menu and background audio

--- starwars.js
+++ starwars.js
@@ -3,3 +3,58 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+function getMoviesList(){
+	return $.ajax({
+		url: 'https://swapi.co/api/films',
+		method: 'GET',
+		success: getMoviesListSuccess
+	});
+}
+
+var moviesResponse = [];
+
+function getMoviesListSuccess(response){
+	moviesResponse = response.results.sort(sortComparer);
+	let moviesList = document.getElementById('movies').querySelector('ul');
+	moviesList.innerHTML = '';
+	for (var i = moviesResponse.length - 1; i >= 0; i--) {
+		let item = document.createElement('li');
+		let movie = moviesResponse[i];
+		item.setAttribute('data-episode-index', i);
+		item.innerHTML = movie.title;
+		item.addEventListener('click', getMovieDescription);
+		moviesList.appendChild(item);
+	}
+}
+
+function getMovieDescription(e) {
+	let id = e.srcElement.attributes['data-episode-index'].value;
+	let movie = moviesResponse[id];
+	let movieDescription = document.getElementsByClassName('reading-animation')[0];
+	movieDescription.innerHTML = 'Episode ' + movie.episode_id + '\n' + movie.opening_crawl;
+	window.localStorage.setItem('last_movie', JSON.stringify(movie));
+}
+
+function sortComparer(a, b){
+	if (a.episode_id < b.episode_id) {
+		return 1;
+	}
+
+	if (a.episode_id > b.episode_id) {
+		return -1;
+	}
+
+	return 0;
+}
+
+function loadLastMovie(){
+	let movie = JSON.parse(window.localStorage.getItem('last_movie'));
+	if (movie) {
+		let movieDescription = document.getElementsByClassName('reading-animation')[0];
+		movieDescription.innerHTML = 'Episode ' + movie.episode_id + '\n' + movie.opening_crawl;
+	}
+}
+
+getMoviesList();
+loadLastMovie();
